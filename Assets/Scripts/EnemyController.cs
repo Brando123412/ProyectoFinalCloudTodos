fd0ffead380cdd41e8f0b26f4b5c5955929ca508
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2023-12-02 00:06:34 -0500

    Update1

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 public class EnemyController : Stats
 {
     [SerializeField] protected PlayerController playerRefences;
     [SerializeField] protected float maxForce;
+    [SerializeField] HealthBarController healthBar;
+    public event Action<int, HealthBarController> onCollision;
 
     protected override void Awake()
     {
         base.Awake();
         playerRefences = FindObjectOfType<PlayerController>();
+        onCollision += DamageCalculation;
     }
     private void FixedUpdate()
     {
         Movement();
        
     }
-    
-    protected virtual void Movement(){}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
 
+            Debug.Log(healthBar.gameObject);
+            //onCollision?.Invoke(10,healthBar);
+            healthBar.UpdateHealth(-10);
+            Debug.Log("Te baje vida");
+        }
+    }
+
+    protected virtual void Movement(){}
+    private void DamageCalculation(int damageTaken, HealthBarController healthBarController)
+    {
+        healthBarController.UpdateHealth(-damageTaken);
+    }
 
 }
--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class HealthBarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int maxValue;
+    [Header("Health Bar Visual Components")]
+    [SerializeField] private RectTransform healthBar;
+    [SerializeField] private RectTransform modifiedBar;
+    [SerializeField] private float changeSpeed;
+
+    public int currentValue;
+    private float _fullWidth;
+    private float TargetWidth => currentValue * _fullWidth / maxValue;
+    private Coroutine updateHealthBarCoroutine;
+
+    public event Action onHit;
+    public event Action onDeath;
+
+    private void Start()
     {
-        
+        currentValue = maxValue;
+        _fullWidth = healthBar.rect.width;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Metodo <c>UpdateHealth</c> actualiza la vida del personaje de manera visual. Recibe una cantidad de vida modificada.
+    /// </summary>
+    /// <param name="amount">El valor de vida modificada.</param>
+    public void UpdateHealth(int amount)
     {
-        
+        currentValue = Mathf.Clamp(currentValue + amount, 0, maxValue);
+        onHit?.Invoke();
+
+        if (updateHealthBarCoroutine != null)
+        {
+            StopCoroutine(updateHealthBarCoroutine);
+        }
+        updateHealthBarCoroutine = StartCoroutine(AdjustWidthBar(amount));
+
+        if (currentValue == 0)
+        {
+            onDeath?.Invoke();
+        }
+        Debug.Log("Estoy mas adentro");
+    }
+
+    IEnumerator AdjustWidthBar(int amount)
+    {
+        RectTransform targetBar = amount >= 0 ? modifiedBar : healthBar;
+        RectTransform animatedBar = amount >= 0 ? healthBar : modifiedBar;
+
+        targetBar.sizeDelta = SetWidth(targetBar, TargetWidth);
+
+        while (Mathf.Abs(targetBar.rect.width - animatedBar.rect.width) > 1f)
+        {
+            animatedBar.sizeDelta = SetWidth(animatedBar, Mathf.Lerp(animatedBar.rect.width, TargetWidth, Time.deltaTime * changeSpeed));
+            yield return null;
+        }
+
+        animatedBar.sizeDelta = SetWidth(animatedBar, TargetWidth);
+    }
+
+    private Vector2 SetWidth(RectTransform t, float width)
+    {
+        return new Vector2(width, t.rect.height);
     }
 }
